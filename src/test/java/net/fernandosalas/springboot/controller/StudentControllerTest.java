f5022fa2b8ad0d899a6db98e5c5b030aac545dfa
krunal.dave <krunal.dave@ukheshe.com>
2024-10-10 16:12:11 +0530

    Commiting unit test changes

--- src/test/java/net/fernandosalas/springboot/controller/StudentControllerTest.java
+++ src/test/java/net/fernandosalas/springboot/controller/StudentControllerTest.java
@@ -62,5 +62,115 @@ public class StudentControllerTest {
                 .andExpect(jsonPath("$.email", is(student.getEmail())));
     }
 
+    @Test
+    public void whenGetAllStudents_thenReturnStudentList() throws Exception {
+        // given - precondition or setup
+        List<Student> students = new ArrayList<>();
+        students.add(Student.builder().firstName("John").lastName("Doe").email("john.doe@example.com").build());
+        students.add(Student.builder().firstName("Jane").lastName("Doe").email("jane.doe@example.com").build());
 
+        given(studentService.getAllStudents()).willReturn(students);
+
+        // when - action or the behavior we are going to test
+        ResultActions response = mockMvc.perform(get(API_PATH)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then - verify the output
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(students.size())));
+    }
+
+    @Test
+    public void givenStudentId_whenGetStudentById_thenReturnStudentObject() throws Exception {
+        // given - precondition or setup
+        long studentId = 1L;
+        Student student = Student.builder().firstName("John").lastName("Doe").email("john.doe@example.com").build();
+
+        given(studentService.getStudentById(studentId)).willReturn(Optional.of(student));
+
+        // when - action or the behavior we are going to test
+        ResultActions response = mockMvc.perform(get(API_PATH + "/{id}", studentId)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then - verify the output
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is(student.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(student.getLastName())))
+                .andExpect(jsonPath("$.email", is(student.getEmail())));
+    }
+
+    @Test
+    public void givenInvalidStudentId_whenGetStudentById_thenReturnEmpty() throws Exception {
+        // given - precondition or setup
+        long studentId = 1L;
+
+        given(studentService.getStudentById(studentId)).willReturn(Optional.empty());
+
+        // when - action or the behavior we are going to test
+        ResultActions response = mockMvc.perform(get(API_PATH + "/{id}", studentId)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then - verify the output
+        response.andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void givenUpdatedStudent_whenUpdateStudent_thenReturnUpdatedStudentObject() throws Exception {
+        // given - precondition or setup
+        long studentId = 1L;
+        Student savedStudent = Student.builder().firstName("John").lastName("Doe").email("john.doe@example.com").build();
+        Student updatedStudent = Student.builder().firstName("Johnny").lastName("Doe").email("johnny.doe@example.com").build();
+
+        given(studentService.getStudentById(studentId)).willReturn(Optional.of(savedStudent));
+        given(studentService.updateStudent(ArgumentMatchers.any(Student.class))).willAnswer((invocation) -> invocation.getArgument(0));
+
+        // when - action or the behavior we are going to test
+        ResultActions response = mockMvc.perform(put(API_PATH + "/{id}", studentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedStudent)));
+
+        // then - verify the output
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is(updatedStudent.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(updatedStudent.getLastName())))
+                .andExpect(jsonPath("$.email", is(updatedStudent.getEmail())));
+    }
+
+    @Test
+    public void givenInvalidStudentId_whenUpdateStudent_thenReturn404() throws Exception {
+        // given - precondition or setup
+        long studentId = 1L;
+        Student updatedStudent = Student.builder().firstName("Johnny").lastName("Doe").email("johnny.doe@example.com").build();
+
+        given(studentService.getStudentById(studentId)).willReturn(Optional.empty());
+
+        // when - action or the behavior we are going to test
+        ResultActions response = mockMvc.perform(put(API_PATH + "/{id}", studentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedStudent)));
+
+        // then - verify the output
+        response.andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void givenStudentId_whenDeleteStudent_thenReturn200() throws Exception {
+        // given - precondition or setup
+        long studentId = 1L;
+        willDoNothing().given(studentService).deleteStudent(studentId);
+
+        // when - action or the behavior we are going to test
+        ResultActions response = mockMvc.perform(delete(API_PATH + "/{id}", studentId)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then - verify the output
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Student Deleted Successfully"));
+    }
 }
